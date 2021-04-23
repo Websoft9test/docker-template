9b976983a50edd1ddad35edcd8b61223cdf114cf
zengxc-1996 <63622990+zengxc-1996@users.noreply.github.com>
2021-04-23 19:54:03 +0800

    Update docker-installer.sh

--- docker-installer.sh
+++ docker-installer.sh
@@ -1,1 +1,213 @@
+#!/bin/bash
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+clear
+
+set -e
+docker_download_url="https://download.docker.com/linux/static/stable/x86_64/docker-20.10.6.tgz"
+
+# Please modify this version and time after update
+version(){
+    echo "version: 1.0"
+    echo "updated date: 20201-04-22"
+}
+ 
+tool_list=(
+git
+pwgen
+jq
+wget
+curl
+)
+
+help_str="
+Usage:
+  -h, --help       Show this help message         
+  -r, --repo_name  The name of the warehouse
+  -p, --package    Making local packs,Only when you need to pack
+  -v, --version    Show version info
+example: bash install.sh -r template -p  
+"
+make_package=false
+
+# get parameter
+getopt_cmd=$(getopt -o r:phv --long repo_name:,package,help,version -n "Parameter error" -- "$@")
+eval set -- "$getopt_cmd"
+
+while [ -n "$1" ]
+do
+    case "$1" in
+        -r|--repo_name)
+            repo_name=$2
+            shift ;;
+        -h|--help)
+            echo -e "$help_str"
+            exit ;;
+        -p|--package)
+            make_package=true
+            shift ;;
+        -v|--version)
+            version
+            shift ;;
+         --) 
+            echo -e "$help_str"
+            break
+            shift ;;
+    esac
+    shift
+done
+
+[ ! -n "$repo_name" ] && exit 0
+echo "docker-$repo_name to be installed,Package as an image: $make_package"
+
+install_tools(){
+  if command -v apt > /dev/null;then  
+    sudo apt update 1>/dev/null 2>&1
+    sudo apt install ${tool_list[*]} -y  1>/dev/null 2>&1
+  elif  command -v yum > /dev/null;then 
+    sudo yum clean all 1>/dev/null 2>&1
+    sudo yum makecache 1>/dev/null 2>&1
+    sudo yum install ${tool_list[*]} -y 1>/dev/null 2>&1
+  fi
+}
+
+install_docker_source(){
+    cd /tmp/
+    rm -rf docker.tgz
+    sudo wget $docker_download_url -O docker.tgz 1>/dev/null 2>&1
+    tar -xf docker.tgz 
+    sudo systemctl stop docker &>/dev/mull || true
+sudo cat > /etc/systemd/system/docker.service <<EOF
+[Unit]
+Description=Docker Application Container Engine
+Documentation=https://docs.docker.com
+After=network-online.target firewalld.service
+Wants=network-online.target
+
+[Service]
+Type=notify
+# the default is not to use systemd for cgroups because the delegate issues still
+# exists and systemd currently does not support the cgroup feature set required
+# for containers run by docker
+ExecStart=/usr/bin/dockerd
+ExecReload=/bin/kill -s HUP $MAINPID
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNOFILE=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+# Uncomment TasksMax if your systemd version supports it.
+# Only systemd 226 and above support this version.
+#TasksMax=infinity
+TimeoutStartSec=0
+# set delegate yes so that systemd does not reset the cgroups of docker containers
+Delegate=yes
+# kill only the docker process, not all processes in the cgroup
+KillMode=process
+# restart the docker process if it exits prematurely
+Restart=on-failure
+StartLimitBurst=3
+StartLimitInterval=60s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    cp docker/* /usr/bin/  
+    sudo systemctl daemon-reload
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo echo "Docker was installed successfully"
+    sudo echo `docker -v` 
+}
+
+install_docker_script(){
+    curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+    sysemctl start docker 
+    systemctl enable docker  
+}
+
+uninstall_docker(){
+    rm -f /etc/systemd/system/docker.service 
+    rm -rf /usr/bin/docker*
+    sudo systemctl daemon-reload
+    sudo echo -e "Docker uninstalled successfully"
+}
+
+install_docker_compose(){ 
+    curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 1>/dev/null 2>&1
+    sudo chmod +x /usr/local/bin/docker-compose 
+    ln -sf /usr/local/bin/docker-compose  /usr/bin/  
+    sudo echo -e "docker-compose installed successfully"
+}
+
+installation(){
+    rm -rf $install_dir
+    mkdir -p $install_dir && cd $install_dir  
+    sudo git clone https://github.com/Websoft9/docker-$repo_name.git $install_dir 
+    #Random password
+    docker-compose -f $compose_file_name up -d 
+}
+
+save_images(){
+    rm -rf /tmp/docker-$repo_name
+    sudo git clone https://github.com/Websoft9/docker-$repo_name.git /tmp/docker-$repo_name
+    sudo docker rmi `docker images -aq` -f &>/dev/null || true   
+    cd /tmp/docker-$repo_name
+    docker-compose -f $compose_file_name pull 
+    echo -e "In image packaging, there is a long wait..." 
+    docker save $(docker images | grep -v REPOSITORY | awk 'BEGIN{OFS=":";ORS=" "}{print $1,$2}') -o /tmp/$repo_name.tar 
+    sudo echo -e "The image was successfully saved as a tar package"
+}
+
+add_install_file(){
+    rm -f /tmp/install.sh
+    sudo echo "new_password=$(pwgen -ncCs 15 1)" | tee -a /tmp/install.sh
+    sudo echo "sudo echo $new_password |tee -a /credentials/password.txt" |tee -a /tmp/install.sh
+sudo cat >> /tmp/install.sh <<EOF
+    docker load -i $repo_name.tar 
+    rm -rf $install_dir /credentials
+    mkdir -p $install_dir /credentials 
+    cd $install_dir && cd .. && PWD=$(pwd) && rm -rf $repo_name
+    /bin/cp -rf docker-$repo_name $PWD/$repo_name 
+    cd $install_dir 
+    # db random password
+    sed -i "s/123456/$new_password/g" $install_dir/$compose_file_name &>/dev/null || true
+    sed -i "s/123456/$new_password/g" $install_dir/.env &>/dev/null || true
+    docker-compose -f $compose_file_name up -d 1>/dev/null 2>&1
+EOF
+}
+
+get_install_information(){
+   install_dir=`curl -s https://raw.githubusercontent.com/Websoft9/docker-$repo_name/main/variables.json |jq -r .installpath` 1>/dev/null
+   compose_file_name=`curl -s https://raw.githubusercontent.com/Websoft9/docker-$repo_name/main/variables.json |jq -r .compose_file` 1>/dev/null
+if [ ! -n "$install_dir" ] && [ ! -n "$compose_file_name" ];then
+       echo "variables.json has an undefined parameter"
+       exit 1 
+fi 
+   echo $install_dir $compose_file_name
+}
+
+make_package(){
+   rm -rf ~/$repo_name.tgz 
+   cd /tmp && tar -zcPf ~/$repo_name.tgz ./{install.sh,$repo_name.tar,docker-$repo_name}
+   cd ~  && sudo echo -e "Image packaging successfully"
+}
+
+install_tools
+
+get_install_information  
+
+if [ $make_package = false ]; then
+   install_docker_script
+   install_docker_compose
+   installation
+fi
+
+if [ $make_package = true ]; then
+   install_docker_source
+   install_docker_compose
+   save_images
+   add_install_file
+   make_package
+fi
+
 
