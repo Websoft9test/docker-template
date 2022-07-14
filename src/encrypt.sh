bd71c76d27a5aad6af97cf3da3f8bcd19d9bd0d9
Darren <27513732@qq.com>
2022-07-14 09:51:14 +0800

    Update encrypt.sh

--- src/encrypt.sh
+++ src/encrypt.sh
@@ -1,1 +1,12 @@
-echo -n '$1' | sha256sum | awk '{ print $1 }'
+#!/bin/bash
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+clear
+
+
+
+# Add encrypt script below if use APP_ENCRYPT_PASSWORD at .env file
+# ------------- start -------------------------
+
+
+
+# ------------- end ---------------------------
